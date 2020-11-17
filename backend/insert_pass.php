c07ecb41e91894afdf3d4c5426cdf0d3787b7e8d
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-17 18:26:50 +0100

    Bo | Spelling corrected & added files

--- backend/insert_pass.php
+++ backend/insert_pass.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    include ("connectgitDB.php");
+    include ("connectDB.php");
     $query = $db->prepare("INSERT INTO users(uname, pswrd) VALUES ('Bo-Eamonn de Snoo','" . sha1('wachtwoord') . "')");
     if($query->execute()){
         echo "nieuwe gegevens zijn toegevoeg;";
