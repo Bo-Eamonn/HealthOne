1301f6c0d2ad472033f989d58b2daecb964e57bf
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-21 00:47:51 +0100

    Bo | Tried adding working searchbar but to tired so imma go sleep <3

--- frontend/web/header.php
+++ frontend/web/header.php
@@ -7,6 +7,8 @@
     <title>HealthOne | Doktor</title>
     <link rel="stylesheet" href="/HealthOne/frontend/static/css/user.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript" src="/healthone/frontend/static/js/search.js"></script>
 </head>
 
 <body>
@@ -14,10 +16,11 @@
         <div>
             <img src="/healthone/frontend/static/images/logo.png" alt="Logo" id="logo">
             <div class="search-container">
-                <form>
-                    <input type="text" placeholder="Zoeken" title="Zoek naar patienten" name="search">
+                <form class="search-box">
+                    <input type="text" placeholder="Zoeken" title="Zoek naar patienten" name="search" autocomplete="off">
                     <button type="submit"><i title="zoeken..." class="fa fa-search"></i></button>
                 </form>
+                <div class="result"></div>
             </div>
             <div>
                 <button id="logout" title="uitloggen"
