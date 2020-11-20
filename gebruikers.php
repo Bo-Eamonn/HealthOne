416cdfd32a55553e14c6bb22cde5cbe392bc001f
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-21 00:12:24 +0100

    Bo | Changes some names and tags

--- gebruikers.php
+++ gebruikers.php
@@ -14,7 +14,7 @@ require_once "frontend/web/nav.php";
                 }
             ?>
             <div class="container">
-                <div class="users">
+                <div class="table">
                     <?php
                         try{
                             include("backend/connectDB.php");
@@ -24,13 +24,13 @@ require_once "frontend/web/nav.php";
                             echo "<table>";
                             echo "<tr>";     
                                 echo "<th>Naam</th>";                       
-                                echo "<th>Cat</th>";   
-                                echo "<th>edit</th>";   
+                                echo "<th>Categorie</th>";   
+                                echo "<th>Edit</th>";   
                             echo "</tr>";                       
                                 foreach ($result as &$data) {
                                 echo "<tr>";    
                                     echo "<td id='" . $data["catDesc"]."'>" . $data["uname"] . " </td>";
-                                    echo "<td id='" . $data["catDesc"]."'>" . $data["cat"] . " </td>";
+                                    echo "<td id='" . $data["catDesc"]."'>" . $data["catDesc"] . " </td>";
                                     echo "<td id='" . $data["catDesc"]."'>" . "<a title='Edit' href='editUser.php?id=" . $data['id'] . "'><i class='fa fa-pen' aria-hidden='true'></i></a>
                                                                                <a title='Delete' href='deleteUSer.php?id=" . $data['id'] . "'><i class='fa fa-trash' aria-hidden='true'></i></a>" . " </td>";
                                 echo "</tr>";
@@ -39,7 +39,6 @@ require_once "frontend/web/nav.php";
                             } catch(PDOException $e){
                             die("Error!: " . $e->getMessage());
                         }
-
                     ?>
                 </div>
             </div>
