f7a7139b21c0e108be628a5b78f8277bb8695ff3
Bo-Eamonn <bo.eamonn@gmail.com>
2020-11-21 00:26:26 +0100

    Bo | Added patients page

--- frontend/web/nav.php
+++ frontend/web/nav.php
@@ -3,7 +3,7 @@
             <ul>
                 <li><button onclick="location.href='home.php'"> Home </button></li>
                 <li><button onclick="location.href='medicijnen.php'"> Medicijnen </button></li>
-                <li><button onclick="location.href='patienten'"> Patiënten </button></li>
+                <li><button onclick="location.href='patienten.php'"> Patiënten </button></li>
                 <li><button onclick="location.href='gebruikers.php'"> gebruikers </button></li>
                 <li><button onclick="location.href='betalen.php'"> Betalen </button></li>
             </ul>
---
+++ patienten.php
@@ -0,0 +1,52 @@
+<?php
+require_once "frontend/web/header.php";
+require_once "frontend/web/nav.php";
+?>
+    <main>
+        <div class="content">
+            <img src="/healthone/frontend/static/images/profilePicture/profilePlaceholder.png" alt="Profiel Foto">
+            <?php
+                session_start();
+                if ($_SESSION['login'] == true) {
+                echo "<h1>Welkom: " . $_SESSION['username'] . "</h1>";
+                } else {
+                    header('Location: /healthOne/');
+                }
+            ?>
+            <div class="container">
+                <div class="table">
+                    <?php
+                        try{
+                            include("backend/connectDB.php");
+                            $query = $db->prepare("SELECT * FROM patienten");
+                            $query->execute();
+                            $result = $query->fetchALL(PDO::FETCH_ASSOC);
+                            echo "<table>";
+                            echo "<tr>";     
+                                echo "<th>Naam</th>";                          
+                                echo "<th>Geboorte datum</th>";                          
+                                echo "<th>Zorg Nummer</th>";                          
+                                echo "<th>Edit</th>";                          
+                            echo "</tr>";                       
+                                foreach ($result as &$data) {
+                                echo "<tr>";    
+                                    echo "<td>" . $data["naam"] . " </td>";
+                                    echo "<td>" . $data["geboortedatum"] . " </td>";
+                                    echo "<td>" . $data["zknummer"] . " </td>";
+                                    echo "<td>" . "<a title='Edit' href='editUser.php?id=" . $data['id'] . "'><i class='fa fa-pen' aria-hidden='true'></i></a>
+                                                                               <a title='Delete' href='deleteUSer.php?id=" . $data['id'] . "'><i class='fa fa-trash' aria-hidden='true'></i></a>" . " </td>";
+                                echo "</tr>";
+                            }
+                            echo "</table>";     
+                            } catch(PDOException $e){
+                            die("Error!: " . $e->getMessage());
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php
+require_once "frontend/web/footer.php";
+?>
